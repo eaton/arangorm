858b5ea160ed5a5b0a072e62f488f4a991c35849
Jeff Eaton <jeff@eaton.fyi>
2024-04-26 13:54:22 -0500

    Wrasslin with persistent storage

--- test/container.ts
+++ test/container.ts
@@ -1,31 +1,34 @@
 import anyTest, { TestFn } from 'ava';
 import { container, DbContext } from '../src/util/get-container.js'
 import { Database } from 'arangojs/database';
+import path from 'path';
+import { rm } from 'fs/promises';
 
 const test = anyTest as TestFn<DbContext>;
 
 test.before(async t => {
-  await container.setup(t.context, { rootPassword: false });
+  await container.setup(
+    t.context,
+    { rootPassword: false },
+    { HostConfig: { Binds: [path.resolve("./test/fixtures/arango") + ":/var/lib/arangodb3"] } }
+  );
   // This really is terrible but Arango needs time to spin up.
   return new Promise(resolve => setTimeout(resolve, 3000));
 });
 
+
 test('has volumes', async t => {
   return t.context.dbContainer.inspect()
     .then(containerData => {
-      t.log(containerData);
-
       t.truthy(containerData.Image, 'has image');
       t.is(containerData.State.Status, 'running', 'is running');
       t.is(Object.keys(containerData.HostConfig.PortBindings).length, 1, 'has exposed port');
-      
-      // Need to get bind mounts working before this will do anything.
-      // t.is(containerData.HostConfig.Mounts?.length, 1, 'has mounted directories');
   });
 })
 
 test.serial('db exists', async t => {
   let db = new Database(t.context.dbConfig);
+
   await db.createDatabase('test');
 
   db = db.database('test');
@@ -46,4 +49,5 @@ test.serial('record exists', async t => {
 
 test.after.always(async t => {
   await container.teardown(t.context);
+  await rm(path.resolve("./test/fixtures/arango"), { recursive: true });
 });
