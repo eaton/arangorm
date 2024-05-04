7f9c4319180f9fa3d1f799e438d96c83fdb0fa31
Jeff Eaton <jeff@eaton.fyi>
2024-05-03 20:51:18 -0500

    The barests of tests

--- test/memory-store.ts
+++ test/memory-store.ts
@@ -4,5 +4,11 @@ import { MemoryStore } from '../src/index.js'
 const ms = new MemoryStore();
 
 test('crud', async t => {
+  const testRecord = { _collection: 'foo', value: 'bar' };
+  await ms.ensureCollection('foo');
+  await ms.save(testRecord);
 
+  const count = await ms.ensureCollection('foo');
+  t.is(count, 1);
+  return Promise.resolve();
 })
